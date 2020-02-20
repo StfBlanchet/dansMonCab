1836d8b999750feebebde6ee82eee6b44566a517
StfBlanchet <47082711+StfBlanchet@users.noreply.github.com>
2020-02-20 06:43:27 +0100

    Improve relevance, extend data extraction

--- dansMonCab/parser.py
+++ dansMonCab/parser.py
@@ -2,157 +2,188 @@
 # coding: utf-8
 
 """
-dansMonCab chatbot
+dansMonCab scraper 
 """
 
 
-from dansMonCab.reply import *
-from dansMonCab.sources import *
+from modules.reply import *
+from modules.sources import *
+from modules.process import *
 
 import random
-from bs4 import BeautifulSoup as bS
 import requests
-import re
-from unidecode import unidecode
-
-import spacy
-nlp = spacy.load('fr_core_news_sm')
-
-
-"""
-This class allows to process user input
-and to provide her with the information she asks for.
-"""
+from bs4 import BeautifulSoup as bS
 
 
 class Driver:
 
     def __init__(self, user_input):
-        self.input = user_input
+        self.entry = user_input
+        self.input = format_entry(self.entry)
+        self.query = format_query(self.input)
         self.reply = str()
         self.end_reply = random.choice(next)
-        self.status = str()
-        self.ner = str()
         self.entity = str()
+        self.suggest = str()
+        self.brief = str()
+        self.info = str()
         self.address = str()
-        self.search = str()
+        self.city = str()
         self.g_map = str()
-        self.page_id = int()
+        self.brief = str()
         self.extract = str()
+        self.ner = []
         self.link = str()
+        self.news = {}
 
-    def parse(self):
-        """
-        Method to extract named entities
-        from the user's input.
-        """
-        parser = {}
-        doc = nlp(self.input.lower())
-        if len(doc.ents) == 0:
-            self.reply = random.choice(nok)
-            self.status = 'nok'
-        else:
-            for ent in doc.ents:
-                parser.update({'entity': ent.text, 'type': ent.label_})
-            self.ner = unidecode(parser['entity'])
-            self.get_coord()
-            return self.ner
+    def get_entity(self):
 
-    def get_coord(self):
-        """
-        Method to get proper entity
-        name and address from Google results.
-        """
-        terms = self.ner.replace(' ', '+').replace('&', '%26')
-        uri = GOO + terms + "+adresse"
-        res = requests.get(uri).text
+        # Scraping pattern 1
+        res = requests.get(GOO + self.query).text
         soup = bS(res, features='lxml')
-        # pattern 1 i.e. entry is correct and has 1 address
-        soup_1_entity = soup.find_all("span", class_="BNeawe tAd8D AP7Wnd", limit=2)
-        soup_1_address = soup.find_all("div", class_="BNeawe iBp4i AP7Wnd", limit=2)
-        if not soup_1_address:
-            # try pattern 1b i.e. inverted tags
-            soup_2_entity = soup.find_all("div", class_="BNeawe deIvCb AP7Wnd", limit=1)
-            soup_2_address = soup.find_all("span", class_="BNeawe tAd8D AP7Wnd", limit=1)
-            if not soup_2_address:
-                # try pattern 2 i.e. multiple addresses available
-                soup_3_bloc = soup.find_all("div", class_="BNeawe tAd8D AP7Wnd", limit=1)
-                if not soup_3_bloc:
-                    # no soup 3: stop there
-                    self.reply = random.choice(nok)
-                    self.status = 'nok'
-                else:
-                    data = soup_3_bloc[0].get_text().split(',')
-                    if self.ner not in unidecode(data[0].lower()):
-                        # soup 2 is wrong: ask Wiki
-                        self.entity = self.ner
-                        self.reply = random.choice(fuzzy_map)
-                    else:
-                        # soup 3 succeeds:
-                        self.entity = data[0].replace('à proximité de ', '')
-                        self.address = data[1] + ',' + data[2]
-                        self.reply = random.choice(doubt) + " {} se trouve {}.".format(
-                            self.entity, data[1] + " à" + data[2])
-            else:
-                # soup 2 succeeds:
-                self.address = soup_2_address[0].get_text()
-                self.entity = soup_2_entity[0].get_text()
-                self.reply = random.choice(ok) + " {} se trouve {}.".format(self.entity, self.address)
+        try:
+            entity_1 = soup.find_all("div", class_="BNeawe deIvCb AP7Wnd", limit=1)[0].get_text()
+        except IndexError:
+            self.entity = ''
+            self.reply = random.choice(nok)
         else:
-            # soup 1 succeeds:
-            self.address = soup_1_address[0].get_text()
-            self.entity = soup_1_entity[0].get_text()
-            self.reply = random.choice(ok) + " {} se trouve {}.".format(self.entity, self.address)
-        self.clean_terms()
+            if entity_strict_match(self.input, entity_1) is not None:
+                self.entity = entity_1
+                print("entity 1 :", self.entity)
+                self.refresh_query()
+            else:
+                # Scraping pattern 2
+                res = requests.get(GOO + self.query + '+adresse').text
+                soup = bS(res, features='lxml')
+                try:
+                    entity_2 = soup.find_all("div", class_="BNeawe deIvCb AP7Wnd", limit=3)[2].get_text()
+                except IndexError:
+                    self.entity = ''
+                    self.reply = random.choice(nok)
+                else:
+                    if entity_lazy_match(self.input, entity_2) is not None:
+                        self.entity = entity_2
+                        print("entity 2 :", self.entity)
+                    else:
+                        self.reply = "Cherchez-vous {} ?".format(entity_1)
 
-    def clean_terms(self):
-        """
-        Method to properly format search terms
-        for both Google maps and Wikipedia API.
-        """
-        self.search = self.entity.replace(' ', '%20').replace("'", '%20').replace('&', '%26')
-        cleaner = '|' or '/' or ','
-        self.search = self.search.split(cleaner)[0]
-        self.get_map()
+                    self.refresh_query()
+
+    def refresh_query(self):
+        base = format_entry(self.entity)
+        self.query = format_query(' '.join(base.split()))
+        self.get_more()
+
+    def get_more(self):
+        # Scraping pattern 1
+        res = requests.get(GOO + self.query).text
+        soup = bS(res, features='lxml')
+        info_soup = soup.find_all("div", class_="BNeawe s3v9rd AP7Wnd", limit=8)
+        info_txt = [format_goo_txt(l.get_text()) for l in info_soup]
+        info = []
+        for elt in info_txt:
+            if elt.startswith('Adresse'):
+                self.address = elt.split(': ')[1]
+            if elt.startswith('Siège social'):
+                self.city = elt.split(': ')[1]
+                info.append(elt)
+            if elt.startswith('PDG'):
+                info.append(elt)
+            if elt.startswith('Dirigeants'):
+                info.append(elt)
+            if elt.startswith('Actionnaires'):
+                info.append(elt)
+            if elt.startswith('Filiales'):
+                info.append(elt)
+            if elt.startswith('Revenus') or elt.startswith("Chiffre d'affaires"):
+                info.append(elt)
+            if elt.startswith('Effectif') or elt.startswith("Nombre d'employés"):
+                info.append(elt)
+            if elt.startswith('Fondateurs') or elt.startswith('Fondateur'):
+                info.append(elt)
+            if elt.startswith('Création'):
+                info.append(elt)
+        self.info = '<br>'.join(info)
+        if len(self.address) > 0:
+            self.city = self.address.split()[-1]
+        else:
+            self.city = self.city
+        if len(self.city) > 0:
+            self.get_wiki()
+        else:
+            # Scraping pattern 2
+            res = requests.get(GOO + self.query + '+adresse').text
+            soup = bS(res, features='lxml')
+            loc_soup = soup.find_all("div", class_="BNeawe tAd8D AP7Wnd", limit=5)[1:5]
+            loc_txt = [l.get_text() for l in loc_soup]
+            loc_txt_ = [elt.split()[-1] for elt in loc_txt]
+            cities = [item for item in loc_txt_ if item.istitle()]
+            if len(cities) > 0:
+                self.city = cities[0]
+            else:
+                pass
+            self.get_wiki()
+
+    def get_wiki(self):
+        uri = WIKI + SEARCH + self.query
+        try:
+            res = requests.get(uri).json()['query']['search'][0]
+        except LookupError:     # include KeyError and IndexError
+            self.get_news()
+        else:
+            title = format_entry(res['title'])
+            if entity_lazy_match(title, self.entity) is None:
+                self.get_news()
+            else:
+                uri = WIKI + PAGE + str(res['pageid'])
+                page = requests.get(uri).json()['query']['pages']
+                num = [i for i in page][0]
+                text = page[num]['extract'].replace('etc.', '[etc]').split('.')
+                base = '.'.join(text[0:2])
+                self.brief = re.sub(html_cleaner, '', base)
+                self.extract = '.'.join(text[2:13]) + '...'
+                self.ner = get_ner_(re.sub(html_cleaner, '', self.extract))
+                self.link = page[num]['fullurl']
+                self.get_news()
+
+    def get_news(self):
+        # Scrap news about the entity
+        res = requests.get(G_NEWS + self.query).text
+        soup = bS(res, features='lxml')
+        try:
+            source = soup.find_all("div", class_="kCrYT", limit=6)
+            header = soup.find_all("div", class_="BNeawe vvjwJb AP7Wnd", limit=3)
+            subtitle = soup.find_all("div", class_="BNeawe s3v9rd AP7Wnd", limit=6)
+        except IndexError:
+            self.get_map()
+        else:
+            urls = re.findall(urlreg, str(source), re.M | re.I)
+            sources = [l for l in urls if l.startswith('http')]
+            if (len(header)) > 0:
+                for i in range(len(header)):
+                    try:
+                        self.news.update({i: {
+                            'header': header[i].get_text(),
+                            'source': sources[2*i],
+                            'subtitle': subtitle[2*i+1].get_text()
+                        }})
+                    except IndexError:
+                        self.news.update({i: {
+                            'header': header[i].get_text(),
+                            'source': sources[i],
+                            'subtitle': subtitle[2*i+1].get_text()
+                        }})
+
+                self.get_map()
+
+            else:
+                self.entity = ''
+                self.reply = random.choice(nok)
 
     def get_map(self):
-        """
-        Method to generate Google maps URI.
-        """
-        if self.search:
-            self.g_map = G_MAP + self.search
-            self.get_page_id()
+        if self.city != '':
+            self.g_map = G_MAP + self.query + '+' + format_entry(self.city)
         else:
-            self.status = 'nok'
-            self.reply = random.choice(nok)
-
-    def get_page_id(self):
-        """
-        Method to get the page id
-        of the targeted entity.
-        """
-        uri = WIKI + SEARCH + self.search
-        r = requests.get(uri).json()
-        if not r['query']['search']:
-            # no wiki item neither: stop there
-            self.status = 'nok'
-            self.reply = random.choice(nok)
-        else:
-            self.page_id = r['query']['search'][0]['pageid']
-            self.get_extract()
-
-    def get_extract(self):
-        """
-        Method to get the summary
-        of to the targeted entity.
-        """
-        self.status = 'ok'
-        uri = WIKI + PAGE + str(self.page_id)
-        r = requests.get(uri).json()
-        page = r['query']['pages']
-        num = [i for i in page][0]
-        # remove html from the extract
-        cleaner = re.compile('<.*?>')
-        text = re.sub(cleaner, '', page[num]['extract']).split()
-        self.extract = ' '.join(text[0:80]) + '...'
-        self.link = page[num]['fullurl']
+            q_ = unidecode(self.entity.replace(r, "%20")).split()[0:2]
+            q = format_query(' '.join(q_))
+            self.g_map = G_MAP + q
